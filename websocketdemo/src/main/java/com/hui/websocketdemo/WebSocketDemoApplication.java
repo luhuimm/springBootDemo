6ef734346831382380cd194d53004f10f8e2d1e1
luhuimm <1205337324@qq.com>
2020-03-26 13:13:11 +0800

    websocket chat demo

---
+++ websocketdemo/src/main/java/com/hui/websocketdemo/WebSocketDemoApplication.java
@@ -0,0 +1,29 @@
+package com.hui.websocketdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@SpringBootApplication
+@EnableWebSocket
+@Slf4j
+public class WebSocketDemoApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketDemoApplication.class, args);
+    }
+    
+    /**
+     * 会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
+     * 要注意，如果使用独立的servlet容器，
+     * 而不是直接使用springboot的内置容器，
+     * 就不要注入ServerEndpointExporter，因为它将由容器自己提供和管理。
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
